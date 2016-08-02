28814dd0262931a93049b20bf70c00ff54283b3f
AtsuyaSato <AtsuyaSato@AtsuyaSato-no-MacBook.local>
2016-08-02 11:25:12 +0900

    [add]tweet generated image

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var OAuth = require('oauth').OAuth;
 var session = require('express-session'); // 追加
 var qs = require('querystring');
+var request = require('request');
+var url = require('url');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -72,7 +74,7 @@ var oa = new OAuth (
   "HMAC-SHA1"
 );
 //auth/twitterにアクセスするとTwitterアプリケーション認証画面に遷移します。
-app.get('/auth/twitter', function(req, res){
+app.get('/auth/twitter/', function(req, res){
   oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
     if (error) {
       console.log(error);
@@ -87,56 +89,68 @@ app.get('/auth/twitter', function(req, res){
     }
   });
 });
-
+app.get('/tweet', function(req, res, next){
+  oa.post(
+    "https://upload.twitter.com/1.1/media/upload.json",
+    req.session.oauth.access_token,
+    req.session.oauth.access_token_secret,
+    {"media_data":req.session.twData.imgData.split(",")[1]},
+    function(error, data) {
+      if(error) console.log(require('sys').inspect(error))
+      else{
+        var parsedData = JSON.parse(data);
+        var media_ids = parsedData.media_id_string;
+        oa.post(
+          "https://api.twitter.com/1.1/statuses/update.json",
+          req.session.oauth.access_token,
+          req.session.oauth.access_token_secret,
+          {"status":req.session.twData.tweet,"media_ids":media_ids},
+          function(error, data) {
+            if(error) console.log(require('sys').inspect(error))
+            else{
+              res.redirect('/result.html?text='+req.session.twData.imgText+'&image='+req.session.twData.imgName);
+            }
+          }
+        );
+      }
+    }
+  );
+});
 app.get('/auth/twitter/callback', function(req, res, next){
+  console.log("callback");
   if (req.session.oauth) {
     req.session.oauth.verifier = req.query.oauth_verifier;
     var oauth = req.session.oauth;
     oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier,
     function(error, oauth_access_token, oauth_access_token_secret, results){
+      req.session.oauth.access_token = oauth_access_token;
+      req.session.oauth.access_token_secret = oauth_access_token_secret;
       if (error){
         console.log(error);
         res.send("yeah something broke.");
       } else {
-        req.session.oauth.access_token = oauth_access_token;
-        req.session.oauth.access_token_secret = oauth_access_token_secret;
-        console.log(results);
-        fs.readFile('./Tweet.html', 'UTF-8',
-            function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            }
-        );
+        res.redirect('http://127.0.0.1:3000/tweet');
       }
     });
   } else {
     next(new Error("you're not supposed to be here."));
   }
 });
+
 app.post('/post', function (req, res) {
-  if(req.session.oauth && req.session.oauth.access_token) {
-    var body = '';
-    req.on('data', function (data) {
-      body +=data;
-    });
-    req.on('end',function(){
+  var body='';
+   req.on('data', function (data) {
+       body +=data;
+   });
+   req.on('end',function(){
        var POST =  qs.parse(body);
-       var text = POST.text;
-       oa.post(
-     "https://api.twitter.com/1.1/statuses/update.json",
-     req.session.oauth.access_token,
-     req.session.oauth.access_token_secret,
-     {"status":text},
-     function(error, data) {
-       if(error) console.log(require('sys').inspect(error))
-       else console.log(data)
-     }
-   );
-    });
-  } else {
-    res.send('fail.');
-  }
+       req.session.twData = {};
+       req.session.twData.tweet = unescape(POST.tweet);
+       req.session.twData.imgData = unescape(POST.binary);
+       req.session.twData.imgName = unescape(POST.imgSrc);
+       req.session.twData.imgText = unescape(POST.text);
+       res.redirect('http://127.0.0.1:3000/auth/twitter/')
+   });
 });
 
 app.listen(app.get('port'));
